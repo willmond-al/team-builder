03a20b32aa1a80f5536b08ddd91fadb944d05ab8
willmondal <wbmondal@aol.com>
2020-12-15 15:44:47 -0800

    initial values set and passed down to Form.js

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,30 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
 import Form from './Form';
+import TeamMember from './TeamMember'
+import React, { useState } from 'react';
+
+const initialFormValues = {
+  name: "",
+  email: "",
+  role: "",
+}
+
 
 function App() {
+  const [team, setTeam] = useState([])
+  const [formValues, setFormValues] = useState([initialFormValues])
+
+  const updateForm = (inputName, inputValue) => {
+    setFormValues({
+      ...formValues, 
+      [inputName]:inputValue, 
+    })
+  }
+
+  const submitForm = () =>{
+    const newMember = {
+      name: formValues.name.trim(),
+      email: formValues.email.trim(),
+      role:formValues.role, 
+    }
+    if(!newMember.name || !newMember.email || !newMember.role) return
+
+    setTeam([newMember, ...team])
+    setFormValues(initialFormValues)
+  }
+
+  
   return (
     <div className="container">
       <h1>Team Member List</h1>
-        <Form/>
+        <Form
+        values={formValues}
+        update={updateForm}
+        submit= {submitForm}/>
       </div>
   );
 }
 
 export default App;
 
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div> */}
+     
+
